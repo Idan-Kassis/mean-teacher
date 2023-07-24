6de37113f29bf449f80cd4821858228ad979702d
Idan <idankas@post.bgu.ac.il>
2023-07-24 10:25:30 +0000

    changing datasets.py file for DBT dataset contains labeled and unlabeled data

--- pytorch/mean_teacher/datasets.py
+++ pytorch/mean_teacher/datasets.py
@@ -12,20 +12,17 @@ from .utils import export
 
 
 @export
-def imagenet():
+def DBT():
     channel_stats = dict(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
     train_transformation = data.TransformTwice(transforms.Compose([
         transforms.RandomRotation(10),
-        transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
         transforms.ToTensor(),
         transforms.Normalize(**channel_stats)
     ]))
     eval_transformation = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(**channel_stats)
     ])
@@ -33,29 +30,6 @@ def imagenet():
     return {
         'train_transformation': train_transformation,
         'eval_transformation': eval_transformation,
-        'datadir': 'data-local/images/ilsvrc2012/',
-        'num_classes': 1000
-    }
-
-
-@export
-def cifar10():
-    channel_stats = dict(mean=[0.4914, 0.4822, 0.4465],
-                         std=[0.2470,  0.2435,  0.2616])
-    train_transformation = data.TransformTwice(transforms.Compose([
-        data.RandomTranslateWithReflect(4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(**channel_stats)
-    ]))
-    eval_transformation = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(**channel_stats)
-    ])
-
-    return {
-        'train_transformation': train_transformation,
-        'eval_transformation': eval_transformation,
-        'datadir': 'data-local/images/cifar/cifar10/by-image',
-        'num_classes': 10
+        'datadir': '/workspace/DBT_US_Soroka/semi-supervised_data/mt-git-data',
+        'num_classes': 2
     }

70bf53095acbbcfc361e485e25902f46df99f8e0
Idan-Kassis <93663187+Idan-Kassis@users.noreply.github.com>
2023-07-27 13:03:34 +0300

    Update main.py
    Change the EMA updating because add_ is deprecated.

--- pytorch/main.py
+++ pytorch/main.py
@@ -193,7 +193,7 @@ def update_ema_variables(model, ema_model, alpha, global_step):
     # Use the true average until the exponential average is more correct
     alpha = min(1 - 1 / (global_step + 1), alpha)
     for ema_param, param in zip(ema_model.parameters(), model.parameters()):
-        ema_param.data.mul_(alpha).add(1 - alpha, param.data)
+        ema_param.data = alpha * ema_param.data + (1-alpha) * param.data
 
 
 def train(train_loader, model, ema_model, optimizer, epoch, log):
